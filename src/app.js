0762d2a099e954645aa40f510697ecabbbbb1e55
ZachCodedThat <Zachemailsback@gmail.com>
2021-04-16 11:37:09 -0400

    new stage

--- src/app.js
+++ src/app.js
@@ -1,34 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./searchParams";
 
 const App = () => {
-  //   return React.createElement("div", {}, [
-  //     React.createElement("h1", {}, "Adopt Me!"),
-  //     React.createElement(Pet, {
-  //       name: "Marty",
-  //       animal: "Dog",
-  //       breed: "Pitbull",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Jazz",
-  //       animal: "Cat",
-  //       breed: "Mixed",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Toot",
-  //       animal: "Bird",
-  //       breed: "Cockatiel",
-  //     }),
-  //   ]);
-  // };
-
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Marty" animal="Dog" breed="Pitbull" />
-      <Pet name="Jazz" animal="Cat" breed="Mixed" />
-      <Pet name="Hoot" animal="Bird" breed="Canary" />
+      <SearchParams />
     </div>
   );
 };
