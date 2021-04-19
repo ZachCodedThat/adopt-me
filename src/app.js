3ba24eaf54f379f25869bee185501bcbeb69b2a2
ZachCodedThat <Zachemailsback@gmail.com>
2021-04-18 22:05:06 -0400

    added API pull

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ import SearchParams from "./searchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
@@ -15,3 +17,4 @@ render(<App />, document.getElementById("root"));
 
 // This is the highest level of this program where the const App is rendered to the index.html using document.getElementById("root") which looks for the
 // Id of root within the index.html document
+// React.StrictMode disables features the react dev team is phasing out of the production build furture proofing in a way your code.
--- src/searchParams.js
+++ src/searchParams.js
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useEffect, useState } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./results";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
-  const [breeds, setBreeds] = useState([]);
+  const [location, setLocation] = React.useState("Seattle, WA");
+  const [breeds, setBreeds] = React.useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "animal", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreeds, setBreed]);
 
   //   This is a react hook and they all start with "use"
   // "Const SearchParamas" takes these 4 consts and creates 3 seperate
@@ -15,24 +37,34 @@ const SearchParams = () => {
   // paramerters into one generic component that fills itslef in according
   // to what information is called and then can be used to push to the webpage.
   // see "Animal and Breed Dropdown"
+  // The useEffect only runs after render when [animal, setBreeds, setBreed] updates. and does not run on the first render but is scheduled to the next render cycle.
+  // You can use a dependency array of [] to tell a useEffect to run on mount and never again
 
   return (
     <div className="search-params">
-      <h1>{location}</h1>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           location
           <input
             id="location"
             value={location}
             placeholder="location"
-            onChange={(e) => setLocation(e.target.value)} //where is .target pointing? possibly at location because of where it was called ?
+            onChange={(event) => {
+              console.log(event);
+              setLocation(event.target.value);
+            }} //where is .target pointing? possibly at location because of where it was called ?
           />
         </label>
         <AnimalDropdown />
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
