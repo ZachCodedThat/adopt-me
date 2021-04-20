6c15bbe3d7f739babd53edc11a6fe5742cc9ff8a
ZachCodedThat <Zachemailsback@gmail.com>
2021-04-19 21:12:32 -0400

    completed details page and added babel/parcel configs

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,53 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = (props) => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)} </code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  state = { loading: true };
+
+  //   constructor(props) {
+  //     super(props);
+
+  //     this.state = {
+  //       loading: true,
+  //     };
+  //   }
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    }, console.error);
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    const { animal, breed, location, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
+
+// componentDidMount runs like useEffect where it runs on start up and never again
+// information passed to this.props is in a read only format when it reaches there.
+// componentDidMount builds the information we need from the API and then shuts off the loading state.
+// The render throws a loading screen while the API is called then a constant is made containing the neatly organized information from the componentDidMount
+// it then creates markup that dispalays the information as wanted.
